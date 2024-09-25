c31d5c6a91d14ba5820873e54cacc63842b5f81e
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-09-25 20:28:25 +0100

    Feat: add unit test

--- core/src/main/java/org/alten/core/CoreApplication.java
+++ core/src/main/java/org/alten/core/CoreApplication.java
@@ -3,7 +3,7 @@ package org.alten.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.alten.*"})
 public class CoreApplication {
 
     public static void main(String[] args) {
